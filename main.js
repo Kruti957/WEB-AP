63425de2abd0eff4229df0584ae0062eebeb7366
Kruti957 <100508828+Kruti957@users.noreply.github.com>
2022-05-27 14:52:34 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,37 @@
+moustachX=0;
+moustacheY=0;
+
+function preload(){
+
+}
+
+function setup(){
+    canvas=createCanvas(300,300);
+    canvas.center();
+    video=createCapture(VIDEO);
+    video.size(300,300);
+    video.hide;
+    poseNet.on("pose",GetPoses);
+    poseNet = ml5.poseNet(video, modelLoaded);
+}
+
+function modelLoaded(){
+    console.log("THE POSENET IS INITIALIZED");
+}
+function draw(){
+Image(VIDEO,0,0,400,400);
+
+}
+
+function take_snapshot(){
+save("MyFilterImage");
+}
+
+function GetPoses(results){
+    if(results.lenght>0){
+console.log(results);
+moustacheX=results[0].pose.moustache.x;
+moustacheY=results[0].pose.moustache.y;
+console.log("moustacheX= "+ moustacheX+"moustacheY= "+ moustacheY);
+}
+}
